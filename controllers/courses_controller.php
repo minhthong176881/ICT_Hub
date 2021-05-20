8ab5c849d84020e26c15a0b655160ff1dff220e1
nhonlv <nhonlv.soict@gmail.com>
2021-05-20 22:10:17 +0700

    First commit

--- controllers/courses_controller.php
+++ controllers/courses_controller.php
@@ -2,10 +2,7 @@
 require_once 'controllers/base_controller.php';
 
 class CoursesController extends BaseController {
-    function __construct()
-    {
-        $this->folder = 'courses';
-    }
+    
 
     
 }
