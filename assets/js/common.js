9fb3430581a7326bf362ecdd63f14b51295483ed
nhonlv <nhonlv.soict@gmail.com>
2021-06-07 13:11:01 +0700

    minor fix

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,19 +1,19 @@
 
-function xhrPost (postInfo) {
+function xhrPost (url, data, success, error) {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", postInfo.url, true);
+    xhr.open("POST", url, true);
 
     //Send the proper header information along with the request
     // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
     xhr.onreadystatechange = function() { // Call a function when the state changes.
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            if (postInfo.success) {
-                postInfo.success(this.responseText);
+            if (success) {
+                success(this.responseText);
             }
         } else if (this.readyState == XMLHttpRequest.DONE && this.status !== 200) {
-            if (postInfo.error) {
-                postInfo.error(this.status, this.responseText);
+            if (error) {
+                error(this.status, this.responseText);
             }
         }
     }
--- common/utility.php
+++ common/utility.php
@@ -13,16 +13,14 @@ class Utility {
     
         file_put_contents('.debug', "Debug Objects: '" . $result . "'\n", FILE_APPEND);
     }
-<<<<<<< HEAD
-    static function returnResult($data) {
-        echo '{"result":"' . $data . '"}';
-=======
 
     static function gmdateToLocalDate($dateTime){
         /* $gmdate must be in YYYY-mm-dd H:i:s format*/
         $userTz = new \DateTimeZone(date_default_timezone_get());
         $dateTime->setTimeZone($userTz);
         return $dateTime->format('D, j M, Y \a\\t H:i');
->>>>>>> master
+    }
+    static function returnResult($data) {
+        echo '{"result":"' . $data . '"}';
     }
 }
